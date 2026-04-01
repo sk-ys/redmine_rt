627dfba4b80b81f078122623937303473416580c
sk-ys <yskrmt@gmail.com>
2026-04-01 11:00:31 +0000

    Use Redmine's core history template instead of custom views

--- lib/redmine_rt/journals_controller_patch.rb
+++ lib/redmine_rt/journals_controller_patch.rb
@@ -1,5 +1,5 @@
 module RedmineRt
-  # Patches Redmine's JournalsController. 
+  # Patches Redmine's JournalsController.
   module JournalsControllerPatch
 
     def self.included(base) # :nodoc:
@@ -34,23 +34,19 @@ module RedmineRt
         @journal.indice = @journal.indice || @journal.issue.visible_journals_with_index.find{|j| j.id == @journal.id}.indice
 
         if @journal.private_notes
-          if @journal.user != User.current and !User.current.allowed_to?(:view_private_notes, @issue.project) 
+          if @journal.user != User.current and !User.current.allowed_to?(:view_private_notes, @issue.project)
             render plain: "you don't have permission", status: 401
             return
           end
         end
 
-        @reply_links = @issue.notes_addable? 
+        @reply_links = @issue.notes_addable?
 
         respond_to do |format|
           format.html {
             headers["X-issue-lock-version"] = @issue.lock_version.to_s
-      
-            if Rails::VERSION::MAJOR >= 5
-              render :action => 'show', :layout => false, locals: { journal: @journal, issue: @issue, reply_links: @reply_links}
-            else
-              render :action => 'show_old', :layout => false, locals: { journal: @journal, issue: @issue, reply_links: @reply_links}
-            end
+
+            render :partial => 'issues/tabs/history', :layout => false, locals: { tab: { locals: { issue: @issue, journals: [@journal] } } }
           }
           format.api { render plain: {journal: @journal, details: @journal.details}.to_json }
         end
